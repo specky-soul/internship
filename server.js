ea53c23fcde8cf4253f3103f988f3e682e9ababa
specky-soul <spectacularspecky3@gmail.com>
2025-05-16 16:27:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ const port = 3000;
 
 // Connect to MongoDB
 mongoose.connect("mongodb+srv://spectacularspecky3:anjana@cluster0.fr98hl7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
   
 });
 
